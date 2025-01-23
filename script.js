96570be2655074fb8e991b457306b65f5d6d272a
jupiteru <96691352+jupiteru@users.noreply.github.com>
2025-01-23 20:32:24 +0200

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+ // JavaScript для першої кнопки
+ document.getElementById("btnSite").addEventListener("click", function() {
+    window.location.href = "https://jupiter1323.github.io/IC-34_WebLab1_Motliuk/";
+});
